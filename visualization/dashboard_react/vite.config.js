b9e6c940c03446bb7b1f27098fed9c22b046c30d
calico21 <alexrevillaperez@gmail.com>
2026-03-29 18:07:05 +0200

    Actualizar vite.config.js

--- visualization/dashboard_react/vite.config.js
+++ visualization/dashboard_react/vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  base: './',
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-});
+    // Aumenta el límite de advertencia a 2000 KB (2 MB)
+    chunkSizeWarningLimit: 2000,
+    
+    // Opcional: Divide el código para que las librerías pesadas (como React y Recharts) 
+    // vayan en un archivo separado de tu código del Project-GP
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+})
